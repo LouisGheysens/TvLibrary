b226756609c5ddd75d311fee44bd5814e4edf5db
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2022-03-12 16:03:28 +0100

    Fix route bug

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,39 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const apiRoute = require('./apiRoutes');
 
 //@desc GET ALL MOVIES
 //@route GET api/movies
 //@access Public
-router.get(apiRoute.Get, async (req, res) => {
+router.get('/', (req, res) => {
 
 })
 
 //@desc GET MOVIE
 //@route GET api/movies/id
 //@access Public
-router.get(apiRoute.Id, async (req, res) => {
+router.get('/:id', (req, res) => {
     
 })
 
 //@desc Post MOVIE
 //@route POST api/movies/save
 //@access Public
-router.post(apiRoute.Save, async (req, res) => {
+router.post('/api/movies', (req, res) => {
     
 })
 
 //@desc PUT MOVIE
 //@route PUT api/movies/id
 //@access Public
-router.patch(apiRoute.Put, async (req, res) => {
+router.patch('/:id', (req, res) => {
     
 })
 
 //@desc DELETE MOVIE
 //@route DELETE api/movies/id
 //@access Public
-router.delete(apiRoute.Delete, async (req, res) => {
+router.delete('/:id', (req, res) => {
     
 })
 
